20d5ce19533682ba85171be816bf1dfecc2ec413
Syed Parvez Antor <antorparvez@gmail.com>
2021-04-06 23:17:54 +0600

    code modify and package name change

--- app/src/main/java/com/example/androidpracticedelligram/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticedelligram/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.androidpracticedelligram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidpracticedelligram.Activity_01_TextView_EditText_Button.Activity_textview
-import com.example.androidpracticedelligram.activity_02_checkbox_seekbar_switch_radio_dropdown.Activity_checkbox
-import com.example.androidpracticedelligram.activity_03_webview.Activity_webview
-import com.example.androidpracticedelligram.activity_04_picker.PickerActivity
+import com.example.androidpracticedelligram.ui_textview.TextViewActivity
+import com.example.androidpracticedelligram.ui_checkbox.CheckboxActivity
+import com.example.androidpracticedelligram.ui_webview.WebviewActivity
+import com.example.androidpracticedelligram.ui_datepicker.PickerActivity
 import com.example.androidpracticedelligram.databinding.ActivityMainBinding
 
 
@@ -23,16 +23,16 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btn1.setOnClickListener {
-            val intent = Intent(this, Activity_textview::class.java)
+            val intent = Intent(this, TextViewActivity::class.java)
             startActivity(intent)
 
         }
         binding.btn2.setOnClickListener {
-            val intent = Intent(this, Activity_checkbox::class.java)
+            val intent = Intent(this, CheckboxActivity::class.java)
             startActivity(intent)
         }
         binding.btn3.setOnClickListener {
-            val intent = Intent(this, Activity_webview ::class.java)
+            val intent = Intent(this, WebviewActivity ::class.java)
             startActivity(intent)
         }
         binding.datePickerBtn.setOnClickListener {
