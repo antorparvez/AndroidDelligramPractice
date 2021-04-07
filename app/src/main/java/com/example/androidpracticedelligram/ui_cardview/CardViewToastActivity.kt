ef9187cc83753a87434a77dcc3e7bb802304068b
Syed Parvez Antor <antorparvez@gmail.com>
2021-04-07 10:54:19 +0600

    Cardview viewBind set and get text

--- app/src/main/java/com/example/androidpracticedelligram/ui_cardview/CardViewToastActivity.kt
+++ app/src/main/java/com/example/androidpracticedelligram/ui_cardview/CardViewToastActivity.kt
@@ -3,11 +3,13 @@ package com.example.androidpracticedelligram.ui_cardview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.TextView
 import android.widget.Toast
+import com.example.androidpracticedelligram.BaseActivity
 import com.example.androidpracticedelligram.R
 import com.example.androidpracticedelligram.databinding.ActivityCardviewBinding
 
-class CardViewToastActivity : AppCompatActivity() {
+class CardViewToastActivity : BaseActivity() {
 
     lateinit var binding: ActivityCardviewBinding
 
@@ -16,20 +18,55 @@ class CardViewToastActivity : AppCompatActivity() {
         binding = ActivityCardviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initToolbar("Custom Toast, Cardview")
 
+        setValueByViewBinding()
 
         binding.customTastBtn.setOnClickListener {
-            customToast()
+            customToast("This ia a custom Toast")
         }
     }
 
-    private fun customToast() {
-        val layout = layoutInflater.inflate(R.layout.custom_toast,findViewById(R.id.linearLayout))
+    private fun setValueByViewBinding() {
 
+        binding.cardViewSubmitButton.setOnClickListener {
+            val cardviewET = binding.cardViewET.text
+
+
+            if (isNullOrEmpty(cardviewET.toString()))
+
+                customToast("Please write something")
+
+            else
+            {
+                binding.cardViewTV.text = cardviewET
+                binding.cardViewET.setText("")
+            }
+
+        }
+
+
+    }
+
+    private fun customToast(toastMsg: String) {
+
+        val layout = layoutInflater.inflate(R.layout.custom_toast,findViewById(R.id.linearLayout))
+        val tv : TextView = layout.findViewById(R.id.custom_toast_message)
+        tv.text = toastMsg
         val myToast = Toast(applicationContext)
         myToast.duration = Toast.LENGTH_LONG
+        myToast.setText("Asdfasdffdsa")
         myToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
         myToast.view = layout//setting the view of custom toast layout
         myToast.show()
     }
+
+
+    fun isNullOrEmpty(str: String?): Boolean {
+        if (str != null && str.trim().isNotEmpty())
+            return false
+        return true
+    }
+
+
 }
