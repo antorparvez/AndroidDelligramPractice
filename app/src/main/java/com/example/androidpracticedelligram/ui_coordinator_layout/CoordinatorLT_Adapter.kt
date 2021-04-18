fd93f470d3a75a8432563749114bf71badb61599
Syed Parvez Antor <antorparvez@gmail.com>
2021-04-19 04:45:40 +0600

    CoordinatorLayout with recyclerview and viewBinding

---
+++ app/src/main/java/com/example/androidpracticedelligram/ui_coordinator_layout/CoordinatorLT_Adapter.kt
@@ -0,0 +1,50 @@
+package com.example.androidpracticedelligram.ui_coordinator_layout
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidpracticedelligram.R
+import com.example.androidpracticedelligram.databinding.CartItemBinding
+import com.example.androidpracticedelligram.ui_constraint_layout.Item
+
+class CoordinatorLT_Adapter(
+    private val cartList: ArrayList<Item>,
+    private val onItemPositionClick: (position: Int) -> Unit,
+): RecyclerView.Adapter<CoordinatorLT_Adapter.CartViewHolder>() {
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CartViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        val item = cartList[position]
+        holder.binding.cartItemName.text = item.getName()
+        holder.binding.cartItemOffer.text = item.getOffer()
+        holder.binding.rowImageView.setImageResource(item.getImage())
+        holder.binding.cartItemPrice.text = item.getPrice()
+
+    }
+
+    override fun getItemCount(): Int {
+        return cartList.size
+    }
+
+
+
+
+    //View holder inner class
+    inner  class CartViewHolder(val binding: CartItemBinding) : RecyclerView.ViewHolder(binding.root){
+        init {
+            binding.cartBtn.setOnClickListener {
+                onItemPositionClick(adapterPosition)
+            }
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/androidpracticedelligram/ui_coordinator_layout/CoordinatorLayoutActivity.kt
+++ app/src/main/java/com/example/androidpracticedelligram/ui_coordinator_layout/CoordinatorLayoutActivity.kt
@@ -2,20 +2,78 @@ package com.example.androidpracticedelligram.ui_coordinator_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidpracticedelligram.R
 import com.example.androidpracticedelligram.databinding.ActivityCoordinatorLayoutBinding
+import com.example.androidpracticedelligram.ui_constraint_layout.Item
 
 class CoordinatorLayoutActivity : AppCompatActivity() {
+    
+    var list : ArrayList<Item> = ArrayList()
+    
+     val adapterCoordinatorLT_Adapter: CoordinatorLT_Adapter by lazy {
+        CoordinatorLT_Adapter(list)
+         {
+             position ->
+             onItemPositionClick(position )
+     }
+     }
+
+    private fun onItemPositionClick(position: Int) {
+
+
+    }
+
+
     lateinit var viewBinding: ActivityCoordinatorLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityCoordinatorLayoutBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        initVal()
+        initList()
+        initRVAdapter()
     }
 
-    private fun initVal() {
+    private fun initList() {
+
+        list.add(
+            Item(
+                "President Backpack",
+                "2999৳",
+                "২ পিস কিনলে পাবেন 1 পিস ফ্রি",
+                R.drawable.bluebag
+            )
+        )
+        list.add(
+            Item(
+                "President Backpack",
+                "2999৳",
+                "২ পিস কিনলে পাবেন 1 পিস ফ্রি",
+                R.drawable.bluebag
+            )
+        )
+        list.add(
+            Item(
+                "President Backpack",
+                "2999৳",
+                "২ পিস কিনলে পাবেন 1 পিস ফ্রি",
+                R.drawable.bluebag
+            )
+        )
+
+    }
+
+    private fun initRVAdapter() {
+
+        viewBinding.coordinatorLTRV.apply {
+            layoutManager = GridLayoutManager(
+                applicationContext,2
+            )
+
+            viewBinding.coordinatorLTRV.adapter = adapterCoordinatorLT_Adapter
+        }
 
 
     }
